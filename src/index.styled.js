61c8ef214a30624a05cba3192b499f9bd18afbae
Denis <fox.ruden@gmail.com>
2022-10-18 19:04:17 +0300

    theme

--- src/index.styled.js
+++ src/index.styled.js
@@ -1,11 +1,6 @@
 import styled from 'styled-components';
-import { App } from 'components/App';
-
 
 export const AppStyled = styled.div`
-background: #000;
-color: tomato;
-font-family:  ${props => props.theme.fonts.circe};
-font-size: ${props => props.theme.fontWeights.normal};
-
-`
+  font-family: ${props => props.theme.fonts.circe};
+  font-size: ${props => props.theme.fontWeights.normal};
+`;
