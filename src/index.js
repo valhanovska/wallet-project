0ddc5c87d3cfa70b5ce5111ad70f9f8aedc41780
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-10-18 20:42:06 +0300

    CurrencyExchange

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { App } from 'components/App';
-import './index.css';
+import { GlobalStyle } from './index.styled';
+
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
+import { App } from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
+        <GlobalStyle />
         <App />
       </BrowserRouter>
     </ThemeProvider>
