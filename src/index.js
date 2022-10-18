87cda52a56cdf4618fcb8eaacea8289d3b63dc90
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-10-18 20:33:58 +0300

    Summary CE

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
-import { ThemeProvider} from "styled-components";
-import {theme} from "./theme"
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
