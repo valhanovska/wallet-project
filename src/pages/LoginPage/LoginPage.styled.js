46d846185d70d8ece80dfe105f05aa1f86ab7062
Denis <fox.ruden@gmail.com>
2022-10-18 18:14:41 +0300

    theme

---
+++ src/pages/LoginPage/LoginPage.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Titel = styled.h1`
+color: inherit;
+/* font-family:  ${props => props.theme.fonts.circe};
+font-size: ${props => props.theme.fontWeights.normal}; */
+
+`
