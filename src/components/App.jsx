bbbe990fa84fca0ca2e5ba4721dce61fe88adef6
Denis <fox.ruden@gmail.com>
2022-10-19 21:52:32 +0300

    selecte

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
+import { getCategories } from '../redux/transactions/operationsTransactions';
+import { SelectCategory } from '../components/SelectCategory/SelectCategory';
 
 const DashboardPage = lazy(() =>
   import('../pages/DashboardPage/DashboardPage')
@@ -10,12 +13,21 @@ const RegisterPage = lazy(() =>
 const LoginPage = lazy(() => import('../pages/LoginPage/LoginPage'));
 
 export const App = () => {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
+
   return (
     
     <Routes>
+      <Route path="/" element={<SelectCategory />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/transactions" element={<DashboardPage />} />
+      <Route path="*" element={<LoginPage />} />
     </Routes>
     
   );
