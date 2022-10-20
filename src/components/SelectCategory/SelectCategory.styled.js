325e5bff28b49fd197f716c5051561f116e41ae0
Denis <fox.ruden@gmail.com>
2022-10-20 13:29:07 +0300

    save

--- src/components/SelectCategory/SelectCategory.styled.js
+++ src/components/SelectCategory/SelectCategory.styled.js
@@ -1,7 +1,32 @@
 import styled from 'styled-components';
 import Select from 'react-select'
 
-export const SelectCategoryStyle = styled(Select)`
-width: 300px ;
+// export const SelectCategoryStyle = styled(Select)`
+// width: 300px ;
+// ::placeholder{
+//     color: red;
+// }
 
+// `
+
+export const Selects = styled.select`
+color: red;
+width: 280px;
+height: 32px;
+font-family: 'Circe';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 27px;
+border:  none;
+
+
+
+
+color: #BDBDBD;
+`
+export const Option = styled.option`
+color: orange;
+background-color:  blue;
+border-radius: 0px
 `
