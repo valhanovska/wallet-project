1a47aa9e546cb7ed9e55f7976f85ee8b7df9dfc1
Serggiyenko Mykyta <sergyenko1995@gmail.com>
2022-10-21 16:44:26 +0300

    tablizza

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -8,6 +8,9 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination, { } from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import { categories } from 'db';
+import { useSelector } from 'react-redux';
+import { getAlltransactions } from 'redux/transactionsController/trControllerSelector';
+
 
 const columns = [
   { id: 'transactionDate', label: 'Date', minWidth: 100 },
@@ -38,30 +41,10 @@ const columns = [
   },
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
-}
-
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
-
 export function HomeTab() {
+
+const transactions = useSelector(getAlltransactions)
+  
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
   
@@ -75,33 +58,32 @@ export function HomeTab() {
   };
 
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden', backgroundColor: "rgba(0, 0, 0, 0)" , border: "0" }}>
-      <TableContainer sx={{ maxHeight: 440 , backgroundColor: "rgba(0, 0, 0, 0)", border: "0"}}>
+    <Paper style={{ width: '100%', overflow: 'hidden', backgroundColor: "rgba(0, 0, 0, 0)" , boxShadow:'none'}}>
+          <TableContainer sx={{ maxHeight: 440, }}>
         <Table stickyHeader aria-label="sticky table" >
-          <TableHead >
+          <TableHead  sx={{ maxHeight: 440 , backgroundColor: "white", borderRadius: "30px", }} >
             <TableRow>
               {columns.map((column) => (
                 <TableCell
                   key={column.id}
                       align={column.align}
-                      style={{ maxHeight: 440 , backgroundColor: "rgba(0, 0, 0, 0)",border: "0" }}
-                //   style={{ minWidth: column.minWidth , }}
+                      style={{ maxHeight: 440 ,  }}
                 >
                   {column.label}
                 </TableCell>
               ))}
             </TableRow>
           </TableHead>
-          <TableBody>
-            {categories
+          <TableBody sx={{ maxHeight: 440 , backgroundColor: "rgba(0, 0, 0, 0)" }}>
+           {categories      //transactions Tut vmesto  categories  //* 
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code} style={{ maxHeight: 440 , backgroundColor: "rgba(0, 0, 0, 0)", borderRadius: "30px",}}>
                     {columns.map((column) => {
                       const value = row[column.id];
                       return (
-                        <TableCell key={column.id} align={column.align} >
+                        <TableCell key={column.id} align={column.align} style={{ maxHeight: 440 , backgroundColor: "rgba(0, 0, 0, 0)", border: "15px", }}>
                           {column.format && typeof value === 'number'
                             ? column.format(value)
                             : value}
@@ -115,9 +97,9 @@ export function HomeTab() {
         </Table>
       </TableContainer>
       <TablePagination
-        rowsPerPageOptions={[10, 50, { value: rows.length , label: 'All' }]}
+        rowsPerPageOptions={[10, 50, { value: transactions.length , label: 'All' }]}
         component="div"
-        count={rows.length}
+        count={transactions.length}
         rowsPerPage={rowsPerPage}
         
         page={page}
---
+++ src/redux/transactionsController/trControllerSelector.js
@@ -0,0 +1,2 @@
+export const getAlltransactions = state =>
+  state.transactionsControllers.allTransactions;
