2df9e35949c63e8cb31cf114c7254df0f407a016
Valentyna Hanovska <riefenstahl.valli@gmail.com>
2022-10-21 16:58:32 +0300

    Update ButtonAddTransactions.styled.js

--- src/components/ButtonAddTransactions/ButtonAddTransactions.styled.js
+++ src/components/ButtonAddTransactions/ButtonAddTransactions.styled.js
@@ -2,20 +2,21 @@ import styled from 'styled-components';
 
 export const Button = styled.button`
   position: fixed;
-  bottom: 10px;
-  right: 10px;
-  margin-top: auto;
+  bottom: 40px;
+  right: 40px;
+  /* margin-top: auto;
   margin-bottom: 40px;
   margin-left: auto;
-  margin-right: 40px;
-  height: 44px;
-  width: 44px;
+  margin-right: 40px; */
   display: flex;
   align-items: center;
   justify-content: center;
-  background: #24cca7;
+  height: 44px;
+  width: 44px;
   border-radius: 50%;
-  font-size: 44px;
-  line-height: 0px;
+  /* font-size: 44px;
+  line-height: 0px; */
   border: 0;
+  background: #24cca7;
+  z-index: 10;
 `;
